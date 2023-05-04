3cc61f2e48dcf788839f86e5437a2fd6d850c62c
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2023-05-04 14:18:08 +0530

    updated api link

--- src/Pages/global_constant.js
+++ src/Pages/global_constant.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://reset-password-server-chakri.herokuapp.com";
+export const API_URL = "https://reset-password-server-chakri.cyclic.app";
 // export const API_URL = "http://localhost:8080";
